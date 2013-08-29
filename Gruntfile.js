779f64da3fc43607b4a1190aac4bde4bcdb3ed70
Adam Timberlake <adam.timberlake@gmail.com>
2013-08-29 01:03:59 +0100

    Added Grunt. Fixes #4

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['dist/javascript/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+
+};
